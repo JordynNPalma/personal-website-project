63e3644f56c3dd663cc446893407019161e932e1
Jordyn Palma <Jordynpalmadesign@gmail.com>
2023-02-13 16:53:06 -0700

    added js file into html and organized column structure fortmatting for modal

---
+++ public_html/script.js
@@ -0,0 +1,8 @@
+// let modal = document.querySelector('#modal');
+// let openModal = document.querySelector('.open-button');
+// let closeModal = document.querySelector('.close-button');
+//
+// openModal.addEventListener('click', () => {
+//     modal.showModal();
+//     console.log('clicked')
+// })
