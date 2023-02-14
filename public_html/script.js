0d658f75b18fd41eb6d9e8f398f37441521bb2dc
Jordyn Palma <Jordynpalmadesign@gmail.com>
2023-02-13 18:42:11 -0700

    added resume button

--- public_html/script.js
+++
@@ -1,8 +0,0 @@
-// let modal = document.querySelector('#modal');
-// let openModal = document.querySelector('.open-button');
-// let closeModal = document.querySelector('.close-button');
-//
-// openModal.addEventListener('click', () => {
-//     modal.showModal();
-//     console.log('clicked')
-// })
