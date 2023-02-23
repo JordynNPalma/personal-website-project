f33cb7381cf90bf0e450b33797dea94c06c4722d
Jordyn Palma <Jordynpalmadesign@gmail.com>
2023-02-23 13:22:20 -0700

    updated project highlights. Removed modal. Added new project highlight images.

---
+++ public_html/script.js
@@ -0,0 +1,8 @@
+// lightbox
+
+document.getElementsByClassName('projectHighlight').onclick = () => {
+    basicLightbox.create(`
+		<img width="1400" height="900" src="https://placehold.it/1400x900">
+	`).show()
+
+}
